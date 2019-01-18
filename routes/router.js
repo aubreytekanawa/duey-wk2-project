db8d3a56bb2c5bdd4dc78ac34d6967f3cf65ba40
Aubrey Te Kanawa <aubreytekanawa@gmail.com>
2019-01-18 14:28:37 +1300

    add extra fields to brick to receive information from payment

--- routes/router.js
+++ routes/router.js
@@ -4,44 +4,40 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
-
 router.get('/', (req, res) => {
   res.render('wall', wall)
 })
 
 router.get('/payment/:id', (req, res) => {
-
   let id = req.params.id
   let brick = getBrickById(id)
-  console.log(brick)
+
   res.render('payment', brick)
 })
 
-router.post('/payment/:id', (req,res) => {
+router.post('/payment/:id', (req, res) => {
   let id = req.params.id
   let brick = getBrickById(id)
   brick.purchased = true
+  brick = Object.assign(brick, req.body);
+  console.log(brick.name)
+  
   res.redirect('/')
 })
 
 router.post('/', (req, res) => {
-  console.log(Object.keys(req.body)[0])
-  let id = Object.keys(req.body)[0]
-//console.log(typeof id)
+   let id = Object.keys(req.body)[0]
   res.redirect('/payment/' + id)
 })
 
-function getBrickById(id){
-  for(let i = 0; i < wall.bricks.length; i++) {
-    for(let j=0; j < wall.bricks[0].length; j++) {
-      if(wall.bricks[i][j].id === Number(id)) {
-
-        return wall.bricks[i][j] 
+function getBrickById(id) {
+  for (let i = 0; i < wall.bricks.length; i++) {
+    for (let j = 0; j < wall.bricks[0].length; j++) {
+      if (wall.bricks[i][j].id === Number(id)) {
+        return wall.bricks[i][j]
       }
     }
   }
 }
 
-
 module.exports = router
-
